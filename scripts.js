a0ce3b8cd7619735030d3eca1ae56a92fb28425a
Watersong89 <watersongordon@gmail.com>
2023-10-18 17:25:27 +0100

    js creates 16 divs

---
+++ scripts.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 16; i++) {
+  const container = document.querySelector('.js-container');
+  let myDiv = document.createElement('div');
+  myDiv.classList.add('myDiv');
+  container.appendChild(myDiv);
+}
