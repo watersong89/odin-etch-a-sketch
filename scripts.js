54dc08c8026defd2a270537838a9dff78d54b3bf
Watersong89 <watersongordon@gmail.com>
2023-10-19 15:59:32 +0100

    Grid clears at each submit, and value limits set

--- scripts.js
+++ scripts.js
@@ -1,24 +1,34 @@
-function generateGrid (rowLength) {
-let totalCells = rowLength * rowLength;
-let percentage = 650 / rowLength;
+function generateGrid(rowLength) {
+  let totalCells = rowLength * rowLength;
+  let percentage = 650 / rowLength;
 
-for (let i = 0; i < totalCells; i++) {
+  for (let i = 0; i < totalCells; i++) {
+    const container = document.querySelector('.js-container');
+    let myDiv = document.createElement('div');
+    myDiv.classList.add('myDiv');
+    container.appendChild(myDiv);
+    myDiv.style.width = `${percentage}px`;
+    myDiv.style.height = `${percentage}px`;
+  }
+}
+
+function clearGrid() {
   const container = document.querySelector('.js-container');
-  let myDiv = document.createElement('div');
-  myDiv.classList.add('myDiv');
-  container.appendChild(myDiv);
-  myDiv.style.width = `${percentage}px`;
-  myDiv.style.height = `${percentage}px`;
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
 }
-}
-
-
 
 const rowLengthSubmitBtn = document.querySelector('.row-length-submit');
 const rowLengthInput = document.querySelector('#row-length');
 
 rowLengthSubmitBtn.addEventListener('click', () => {
+  clearGrid();
   let rowLength = rowLengthInput.value;
+  if (rowLength > 100 || rowLength < 1) {
+    alert('The number must be between 1 and 100.')
+  } else {
   generateGrid(rowLength);
+  }
 })
 
