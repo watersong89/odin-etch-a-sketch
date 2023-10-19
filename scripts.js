40a5dfd7ac69fdcfbfcc12facd381d0f4f21b591
Watersong89 <watersongordon@gmail.com>
2023-10-19 18:26:27 +0100

    Enabled lightening and darkening of colors

--- scripts.js
+++ scripts.js
@@ -3,15 +3,19 @@
 const defaultSize = 16;
 
 let mouseDown = false;
+let lightDarkValue = 5;
 
 const colorWheel = document.querySelector('.color-wheel');
+const colorValue = colorWheel.value;
 const eraserSelector = document.querySelector('#eraser-selector');
 const rainbowSelector = document.querySelector('#rainbow-selector');
 const drawSelector = document.querySelector('#draw-selector');
 const clearBtn = document.querySelector('.clearBtn');
 const rowLengthSubmitBtn = document.querySelector('.row-length-submit');
 const rowLengthInput = document.querySelector('#row-length');
-
+const lightenSelector = document.querySelector('#lighten-selector')
+const darkenSelector = document.querySelector('#darken-selector')
+const noneSelector = document.querySelector('#none-selector')
 
 /*Functions*/
 
@@ -38,6 +42,17 @@ function generateGrid(rowLength) {
         }
       } else return;
     })
+  myDiv.addEventListener('click', () => {
+    if (noneSelector.checked) {
+      return;
+    } else if (lightenSelector.checked) {
+      myDiv.style.backgroundColor = LightenDarkenColor(colorWheel.value, 10);
+      lightDarkValue++;
+    } else if (darkenSelector.checked) {
+      myDiv.style.backgroundColor = LightenDarkenColor(colorWheel.value, -10);
+      lightDarkValue--;
+    }
+  })
   }
 }
 
@@ -57,7 +72,25 @@ function eraseGrid() {
   }
 }
 
-console.log(rowLengthInput.value);
+/*Below function taken from https://css-tricks.com/snippets/javascript/lighten-darken-color/ - still don't understand how it works!*/
+
+function LightenDarkenColor(col, amt) {
+  let usePound = false;
+  if (col[0] == "#") {
+      col = col.slice(1);
+      usePound = true;
+  }
+  let num = parseInt(col,16);let r = (num >> 16) + amt;
+  if (r > 255) r = 255;
+  else if  (r < 0) r = 0;
+  let b = ((num >> 8) & 0x00FF) + amt;
+  if (b > 255) b = 255;
+  else if  (b < 0) b = 0;
+  let g = (num & 0x0000FF) + amt;
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+  return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
 
 /*Event Listeners*/
 
